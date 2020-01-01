bb4129060379b2959df5c246207d193fb28456b0
Javier Palomares <javier.palomares.90@gmail.com>
2019-12-31 20:21:14 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+from gmusicapi import Mobileclient
+from spotipy import Spotify, util, client
+
+
+def get_gpm_lib():
+    pass
+
+def main():
+    gpm_library = get_gpm_lib()
+
+
+if __name__ == '__main__':
+    main()
